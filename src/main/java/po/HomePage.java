28e191fd81c8b351819daedfea141c7b04234c4f
olgabernat <141057027+olgabernat@users.noreply.github.com>
2023-11-06 20:08:24 +0100

    add classes (#1)

--- src/main/java/po/HomePage.java
+++ src/main/java/po/HomePage.java
@@ -4,21 +4,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import webdriver.Singleton;
 
+import static po.HomePageLocators.COPY_RIGHT_LOCATOR;
+import static po.HomePageLocators.SING_IN_BUTTON_LOCATOR;
+
 public class HomePage {
 
-    private String singInButtonLocator = "//a[@data-testid='header-sign-in-button']";
-    private String copyRightLocator = "//div[@class='copyright_text']";
     public void openHomePage() {
         Singleton.getDriver().get("https://www.booking.com/");
     }
 
     public void clickSingInButton(){
-        WebElement singInButtonElement = Singleton.getDriver().findElement(By.xpath(singInButtonLocator));
+        WebElement singInButtonElement = Singleton.getDriver().findElement(By.xpath(SING_IN_BUTTON_LOCATOR));
         singInButtonElement.click();
     }
 
     public String getCopyRightText(){
-        WebElement copyRightElement = Singleton.getDriver().findElement(By.xpath(copyRightLocator));
+        WebElement copyRightElement = Singleton.getDriver().findElement(By.xpath(COPY_RIGHT_LOCATOR));
         String result = copyRightElement.getText();
         return result;
     }
--- src/main/java/webdriver/Singleton.java
+++ src/main/java/webdriver/Singleton.java
@@ -3,6 +3,8 @@ package webdriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class Singleton {
     private static WebDriver driver;
 
@@ -13,6 +15,7 @@ public class Singleton {
         if (driver == null) {
             driver = new ChromeDriver();
             driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
         }
         return driver;
     }
