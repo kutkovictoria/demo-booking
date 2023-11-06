35263a8b07927f0346fcec45dc7f2d6f3961b3f3
Vladimir <golikvladimir@gmail.com>
2023-11-06 21:53:45 +0200

    refactor

--- src/test/java/ui/testing/LoginTest.java
+++ src/test/java/ui/testing/LoginTest.java
@@ -1,16 +1,20 @@
 package ui.testing;
 
 import com.github.javafaker.Faker;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import po.HomePage;
 import po.LoginPage;
 
 public class LoginTest extends BaseTest{
+    private static final Logger logger = LogManager.getLogger();
     @Test
     public void testLoginPage(){
         HomePage homePage = new HomePage();
         homePage.openHomePage();
         homePage.clickSingInButton();
+        logger.info("Open HomePage");
 
         LoginPage loginPage = new LoginPage();
         Faker faker = new Faker();
